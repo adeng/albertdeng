bb649e8332b6779a7ba0b3ab4b669a718106d908
Albert Deng <adeng@media.ucla.edu>
2016-09-08 20:31:22 -0700

    Quick bugfix and refactor

--- js/module/services.js
+++ js/module/services.js
@@ -63,7 +63,7 @@ angular.module('main.services', [])
 		getJSON: function(url) {
 			var deferred = $q.defer();
 
-            var fullrl = 'https://cdn.rawgit.com/adeng/albertdeng/dev/data/' + filename;
+            var fullrl = 'https://cdn.rawgit.com/adeng/albertdeng/dev/data/' + url;
 			$http.get(fullrl).success( function(data, status, headers, config) {
 				deferred.resolve(data);
 			});
