fd1ef596f1efa28ac4536010cff978aaccb9df7b
Albert Deng <adeng@media.ucla.edu>
2016-09-12 08:20:41 -0700

    JSON encode

--- data/scripts/tiingo.php
+++ data/scripts/tiingo.php
@@ -1,16 +1,3 @@
 <?php
-    // $opts = array(
-    //     'http'=>array(
-    //         'method'=>"GET",
-    //         'header'=>"Content-type: application/json" .
-    //                 "Authorization: Token ebe2dbfefbee23e974b77c3dece3eeec685fd510" 
-    //     )
-    // );
-    
-    // $context = stream_context_create($opts);
-
-    // $file = file_get_contents('https://api.tiingo.com/tiingo/daily/googl', false, $context); 
-    // echo json_encode($file);
-    echo "loaded";
-    echo file_get_contents('https://api.tiingo.com/tiingo/daily/googl?token=ebe2dbfefbee23e974b77c3dece3eeec685fd510');
+    echo json_encode(file_get_contents('https://api.tiingo.com/tiingo/daily/googl?token=ebe2dbfefbee23e974b77c3dece3eeec685fd510'));
 ?>
