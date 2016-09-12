25ab85ac7a8354c398c3ef25dd8191bf2e4db088
Albert Deng <adeng@media.ucla.edu>
2016-09-12 09:31:36 -0700

    Quote change for PHP

--- data/scripts/tiingo.php
+++ data/scripts/tiingo.php
@@ -1,4 +1,4 @@
 <?php
-    $url = "https://api.tiingo.com/tiingo/daily/" . $_GET['ticker'] . "?token=ebe2dbfefbee23e974b77c3dece3eeec685fd510";
+    $url = "https://api.tiingo.com/tiingo/daily/" . $_GET["ticker"] . "?token=ebe2dbfefbee23e974b77c3dece3eeec685fd510";
     echo json_encode(file_get_contents($url));
 ?>
