1f24cd45c4468b8a9d39c39a0bedd224576353ce
Albert Deng <adeng@media.ucla.edu>
2016-09-20 22:04:05 -0700

    Finished headerObj

--- data/scripts/analyst.php
+++ data/scripts/analyst.php
@@ -6,7 +6,7 @@
         return array($startPos, $endPos);	
     }
 
-    function processDOM($domStr) {	
+    function processDOM($domStr, $numbersOnly) {	
         $dom = new DOMDocument();
         $dom->loadHTML($domStr);
         $temp = array();		
@@ -15,7 +15,7 @@
             $numRatings = array();
             foreach($node->getElementsByTagName('td') as $td) {
                 $val = $td->nodeValue;
-                if(is_numeric($val)) {
+                if(!$numbersOnly or is_numeric($val)) {
                     array_push($numRatings, $val);
                 }
             }
@@ -24,17 +24,20 @@
         return $temp;
     }
 
-	$input = file_get_contents("http://www.reuters.com/finance/stocks/analyst?symbol=MSFT.O");
+	$input = file_get_contents("http://www.reuters.com/finance/stocks/analyst?symbol=ORCL");
 
     $arar = getPos($input, "Analyst Recommendations and Revisions");
     $ratings = substr($input, $arar[0], $arar[1]); 
 
-    $ratingsArr = processDOM($ratings);
+    $ratingsArr = processDOM($ratings, true);
     
     $ratingsObj = array("buy" => $ratingsArr[1], "outperform" => $ratingsArr[2], "hold" => $ratingsArr[3], "underperform" => $ratingsArr[4], "sell" => $ratingsArr[5], "no" => $ratingsArr[6], "avg" => $ratingsArr[8]);
 
     $cr = getPos($input, "Consensus Recommendations");
     $header = substr($input, $cr[0], $cr[1]);
-    echo $header;
 
+    $headerArr = processDOM($header, false);
+
+    $headerObj = array("consensus" => $headerArr[1][0], "esteps" => substr($headerArr[1][1], 8));
+    echo json_encode($headerObj);
 ?>
