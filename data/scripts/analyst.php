7e9c6ebf866f32e620a689ce73a6bd2a91956b3b
Albert Deng <adeng@media.ucla.edu>
2016-09-20 21:37:31 -0700

    Isolate processDOM function

--- data/scripts/analyst.php
+++ data/scripts/analyst.php
@@ -1,5 +1,4 @@
 <?php
-
     function getPos($array, $str) {
         $pos = strpos($array, $str) + strlen($str);
         $startPos = strpos(substr($array, $pos), "<table") + $pos;
@@ -7,26 +6,35 @@
         return array($startPos, $endPos);	
     }
 
+    function processDOM($domStr) {	
+        $dom = new DOMDocument();
+        $dom->loadHTML($domStr);
+        $temp = array();		
+        foreach($dom->getElementsByTagName('tr') as $node)
+        {
+            $numRatings = array();
+            foreach($node->getElementsByTagName('td') as $td) {
+                $val = $td->nodeValue;
+                if(is_numeric($val)) {
+                    array_push($numRatings, $val);
+                }
+            }
+            array_push($temp, $numRatings);
+        }
+        return $temp;
+    }
+
 	$input = file_get_contents("http://www.reuters.com/finance/stocks/analyst?symbol=MSFT.O");
 
     $arar = getPos($input, "Analyst Recommendations and Revisions");
     $ratings = substr($input, $arar[0], $arar[1]); 
 
-    $dom = new DOMDocument();
-    $dom->loadHTML($ratings);
-
-    $ratingsArr = array();		
-    foreach($dom->getElementsByTagName('tr') as $node)
-    {
-		$numRatings = array();
-        foreach($node->getElementsByTagName('td') as $td) {
-            $val = $td->nodeValue;
-            if(is_numeric($val)) {
-                array_push($numRatings, $val);
-            }
-        }
-		array_push($ratingsArr, $numRatings);
-    }
+    $ratingsArr = processDOM($ratings);
+    
     $ratingsObj = array("buy" => $ratingsArr[1], "outperform" => $ratingsArr[2], "hold" => $ratingsArr[3], "underperform" => $ratingsArr[4], "sell" => $ratingsArr[5], "no" => $ratingsArr[6], "avg" => $ratingsArr[8]);
+
+    $cr = getPos($input, "Consensus Recommendations");
+    $header = substr($input, $cr[0], $cr[1]);
+    echo $header;
 
 ?>
