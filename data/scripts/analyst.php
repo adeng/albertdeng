a03ff3201c7ace33bb9225fc15d60b2516dc05f9
Albert Deng <adeng@media.ucla.edu>
2016-09-20 22:31:34 -0700

    Progress towards consensus estimates

--- data/scripts/analyst.php
+++ data/scripts/analyst.php
@@ -26,6 +26,7 @@
 
 	$input = file_get_contents("http://www.reuters.com/finance/stocks/analyst?symbol=ORCL");
 
+    /* Analyst Recommendations */
     $arar = getPos($input, "Analyst Recommendations and Revisions");
     $ratings = substr($input, $arar[0], $arar[1]); 
 
@@ -33,11 +34,21 @@
     
     $ratingsObj = array("buy" => $ratingsArr[1], "outperform" => $ratingsArr[2], "hold" => $ratingsArr[3], "underperform" => $ratingsArr[4], "sell" => $ratingsArr[5], "no" => $ratingsArr[6], "avg" => $ratingsArr[8]);
 
+    /* Consensus Recommendations */
     $cr = getPos($input, "Consensus Recommendations");
     $header = substr($input, $cr[0], $cr[1]);
 
     $headerArr = processDOM($header, false);
 
     $headerObj = array("consensus" => $headerArr[1][0], "esteps" => substr($headerArr[1][1], 8));
-    echo json_encode($headerObj);
+	
+    /* Consensus Estimates */
+    $cea = getPos($input, "Consensus Estimates Analysis");
+    $estimates = substr($input, $cea[0], $cea[1]);
+
+    $estimatesArr = processDOM($estimates, false);
+    $intersect = array(1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0);
+    $estimatesObj = array();
+    echo json_encode($estimatesArr);
+
 ?>
