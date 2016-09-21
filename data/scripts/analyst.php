5a02f0597b2a76b2be6fa3fa4e4760b52c29ce59
Albert Deng <adeng@media.ucla.edu>
2016-09-20 21:24:44 -0700

    Isolate getPos function

--- data/scripts/analyst.php
+++ data/scripts/analyst.php
@@ -1,14 +1,20 @@
 <?php
-	$arr = file_get_contents("http://www.reuters.com/finance/stocks/analyst?symbol=MSFT.O");
 
-    $pos = strpos($arr, "Analyst Recommendations and Revisions") + strlen("Analyst Recommendations and Revisions");
-	$startPos = strpos(substr($arr, $pos), "<table") + $pos;
-	$endPos = strpos(substr($arr,$startPos), "</table>") + strlen("</table>");
-    $ratings = substr($arr, $startPos, $endPos);
+    function getPos($array, $str) {
+        $pos = strpos($array, $str) + strlen($str);
+        $startPos = strpos(substr($array, $pos), "<table") + $pos;
+        $endPos = strpos(substr($array, $startPos), "</table>") + strlen("</table>");
+        return array($startPos, $endPos);	
+    }
+
+	$input = file_get_contents("http://www.reuters.com/finance/stocks/analyst?symbol=MSFT.O");
+
+    $arar = getPos($input, "Analyst Recommendations and Revisions");
+    $ratings = substr($input, $arar[0], $arar[1]); 
 
     $dom = new DOMDocument();
     $dom->loadHTML($ratings);
-	
+
     $ratingsArr = array();		
     foreach($dom->getElementsByTagName('tr') as $node)
     {
@@ -22,7 +28,5 @@
 		array_push($ratingsArr, $numRatings);
     }
     $ratingsObj = array("buy" => $ratingsArr[1], "outperform" => $ratingsArr[2], "hold" => $ratingsArr[3], "underperform" => $ratingsArr[4], "sell" => $ratingsArr[5], "no" => $ratingsArr[6], "avg" => $ratingsArr[8]);
-
-    echo json_encode($ratingsObj);
 
 ?>
